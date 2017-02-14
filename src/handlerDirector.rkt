096da93645722afe03b1d3ac170fe16dfbc3fb89
Logan Davis <logan.h.g.davis@gmail.com>
2017-02-13 23:08:40 -0500

    added handler for block statements.

--- src/handlerDirector.rkt
+++ src/handlerDirector.rkt
@@ -7,10 +7,23 @@
 (define program-tag-handler
   (lambda (datum)
     (let ([content (rest datum)]
-             [new_program '(program)])
+          [new_program '(program)])
          (for ([item content])
            (set! new_program (append new_program (list (tree-transform item)))))
          new_program)))
+
+(define scope-statement-handler
+  (lambda (datum)
+    (let ([block '('scope-statement)])
+      (for ([item (rest datum)])
+        (if (or (equal? (first item) 'lbrac) (equal? (first item) 'rbrac))
+            (set! block (append block (list item)))
+            (set! block (append block (list (tree-transform item))))))
+     (list block))))
+
+(define statement-handler
+  (lambda (datum)
+    (datum)))
 
 ; Takes some syntax->datum list of original AST and returns translated AST
 (define tree-transform
@@ -25,6 +38,9 @@
        (let-tag-handler datum)]
       ;;;----------------DEFINE STATEMENT------------------------;;;
       [(equal? tag 'define-statement)
-       (definition-tag-handler datum)])))
+       (append (definition-tag-handler datum)
+               (scope-statement-handler (fifth datum)))]
+      ;;;----------------STATEMENT HANDLER-----------------------;;;
+      [(equal? tag 'statment) (statement-handler datum)])))
 
 (provide (all-defined-out))
